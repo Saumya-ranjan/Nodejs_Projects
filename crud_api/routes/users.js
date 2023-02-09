13b1fae5ea793bcba6ec1e338db92431df676736
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2023-02-09 20:45:54 +0530

    crud_api done

--- crud_api/routes/users.js
+++ crud_api/routes/users.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-const users = [{
-    name: "Saumyaranjan",
-    lastname : "Parida",
-    age:"20"
-}]
+import { v4 as uuidv4 } from 'uuid';
+// Make a Controller directory and then Import all the functions from that
+
+
+let users = []
 
 // Browsers Can Only Check Get Request Not Post Request.
 router.get('/' , (req,res)=>{
@@ -12,12 +12,46 @@ router.get('/' , (req,res)=>{
     res.send(users)
 })
 
+// Get the Id of the User and use it to fet specific details
+router.get('/:id' , (req,res)=>{
+    const {id} = req.params
+    const foundUser = users.find((user)=>user.id ===id)
+    res.send(foundUser)
+})
+
+// Creating the User by Post Method
 router.post('/',(req , res)=>{
     const user = req.body;
-    users.push(user);
+    // const a = uuidv4()
+    users.push({...user ,id: uuidv4()});
     // console.log(users)
     res.send("added to database!");
 
 }) // Frontend TO Server
+
+// Using Delete Method to Delete User from the users array
+router.delete('/:id' , (req , res)=>{
+    const {id} = req.params
+    users = users.filter((user)=>user.id !== id)
+    res.send(`The user ID: ${id} is deleted`)
+})
+
+
+// Using Patch Method to Get the ID Variable and then changing the value 
+router.patch('/:id', (req , res)=>{
+    const {id} = req.params
+    const {firstName , lastName , age} = req.body
+    const user = users.find((user)=>user.id === id)
+    if (firstName){
+        user.firstName = firstName
+    }
+    if (lastName){
+        user.lastName = lastName
+    }
+    if(age){
+        user.age = age
+    }
+    res.send("I have Updated the Given Details Properly")
+})
 
 export default router
